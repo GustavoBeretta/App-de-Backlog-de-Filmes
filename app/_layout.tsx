dee961970b36deb37f34bc1406f760954ff0d211
GustavoBeretta <gustavoberettag@gmail.com>
2025-09-21 12:58:41 -0300

    ios movieDetailsScreen back button text

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="movie/[id]" options={{ title: "" }} />
+      <Stack.Screen name="movie/[id]" options={{ headerBackTitle: "Voltar" }} />
     </Stack>
   );
 }
